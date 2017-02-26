0673472335b18d02405e218b56797d79195aaadd
Dana <craciundaniela19@gmail.com>
2017-02-26 14:10:44 +0200

    Getting started with iris dataset

---
+++ iris_tutorial.py
@@ -0,0 +1,27 @@
+# Load libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+# Inspect dataset
+# shape
+print(dataset.shape)
+# peek at first 20 entries
+print(dataset.head(20))
+# descriptions (count, min, max, std, percentiles)
+print(dataset.describe())
